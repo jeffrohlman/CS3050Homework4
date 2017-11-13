886c37759ce5b0e76e533fec1dd6b18102705c8a
jeffrohlman <jcrp9f@mail.missouri.edu>
2017-11-13 17:01:04 -0600

    Initial commit of

---
+++ Homework4/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jeff
+ *
+ * Created on November 13, 2017, 4:58 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "input_error.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
